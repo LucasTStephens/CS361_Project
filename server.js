7a6f619441d44f8cff343917afb5e51864ce3734
LucasTStephens <l.stephens33@me.com>
2024-02-21 15:12:19 -0800

    added databes login

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('*', function (req, res) {
 })
 
 app.listen(3231, () => {
-    console.log(`Server is running on http://flip1.engr.oregonstate.edu:3231/`);
+    console.log(`Server is running on http://flip?.engr.oregonstate.edu:3231/`);
 })
 
 // Code to register a user if they are not already in the system
