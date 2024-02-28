d0b48f8eda9325541ca06d4be1d62fdc2db59ca5
FarisNasse <nassef@oregonstate.edu>
2024-02-28 14:13:01 -0800

    Update testRandomShow.js

--- random-show-service/testRandomShow.js
+++ random-show-service/testRandomShow.js
@@ -4,6 +4,7 @@ const URL = 'http://localhost:3001/random-show';
 async function testRandomShowService() {
     try {
         const response = await fetch(URL);
+        console.log('Requesting data from microservice')
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
